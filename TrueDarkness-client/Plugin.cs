621da2703e6e88656bc89a99329e80a5ea0c2038
D. V. <99197717+Friskzips@users.noreply.github.com>
2024-07-28 20:23:16 +0200

    Add main folder stuff

---
+++ TrueDarkness-client/Plugin.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Friskzips.clientPatch;
+using Friskzips.service;
+using Friskzips.configStuff;
+
+namespace Friskzips;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("ainavt.lc.lethalconfig", BepInDependency.DependencyFlags.SoftDependency)]
+public class TrueDarknessClient : BaseUnityPlugin
+{
+    public static TrueDarknessClient Instance { get; set; }
+
+    public static ManualLogSource Log => Instance.Logger;
+
+    private readonly Harmony _harmony = new(PluginInfo.PLUGIN_GUID);
+
+    public FriskzipsService Service;
+
+    internal static bool nightVision = true;
+
+    public TrueDarknessClient()
+    {
+        Instance = this;
+    }
+
+    internal static new Config Config;
+    private void Awake()
+    {
+        Config = new Config(base.Config);
+        Service = new FriskzipsService();
+
+        Log.LogInfo($"Applying patches...");
+        ApplyPluginPatch();
+        Log.LogInfo($"Patches applied");
+    }
+
+    /// <summary>
+    /// Applies the patch to the game.
+    /// </summary>
+    private void ApplyPluginPatch()
+    {
+        _harmony.PatchAll(typeof(NightVisionOverrideClient));
+
+        if (LethalConfigCompatibility.enabled)
+        {
+            Log.LogInfo($"LethalConfig found!");
+            LethalConfigCompatibility.initLethalConfig();
+
+        }
+    }
+}
---
+++ TrueDarkness-client/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Friskzips
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "Friskzips.TrueDarknessClient";
+        public const string PLUGIN_NAME = "TrueDarknessClient";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
